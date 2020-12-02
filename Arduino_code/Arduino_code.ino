acf5508daf67fc37ba5abbd07c109358e961ae94
AW-AlanWu <alan.92925@gmail.com>
2020-12-02 22:12:47 +0800

    2nd commit, 這次將程式碼整理成多個函式，修正註解(目前編譯成功，但尚未上傳arduino測試)，並且更新README

--- Arduino_code/Arduino_code.ino
+++ Arduino_code/Arduino_code.ino
@@ -9,7 +9,7 @@ SoftwareSerial Serial1(6, 7); // RX, TX
 
 char ssid[] = "MICS_LAB";            // your network SSID (name)
 char pass[] = "nlhsmics306";        // your network password
-int status = WL_IDLE_STATUS;     // the Wifi radio's status
+
 
 char timeServer[] = "time.nist.gov";  // NTP server
 unsigned int localPort = 2390;        // local port to listen for UDP packets
@@ -28,81 +28,15 @@ void setup()
   Serial.begin(9600);
   // initialize serial for ESP module
   Serial1.begin(9600);
-  // initialize ESP module
-  WiFi.init(&Serial1);
-
-  // check for the presence of the shield
-  if (WiFi.status() == WL_NO_SHIELD) {
-    Serial.println("WiFi shield not present");
-    // don't continue
-    while (true);
-  }
-
-  // attempt to connect to WiFi network
-  while ( status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to WPA SSID: ");
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network
-    status = WiFi.begin(ssid, pass);
-  }
-
-  // you're connected now, so print out the data
-  Serial.println("You're connected to the network");
+  
+  InitWiFi();
   
   Udp.begin(localPort);
 }
 
 void loop()
 {
-  sendNTPpacket(timeServer); // send an NTP packet to a time server
-  
-  // wait for a reply for UDP_TIMEOUT miliseconds
-  unsigned long startMs = millis();
-  while (!Udp.available() && (millis() - startMs) < UDP_TIMEOUT) {}
-
-  Serial.println(Udp.parsePacket());
-  if (Udp.parsePacket()) {
-    Serial.println("packet received");
-    // We've received a packet, read the data from it into the buffer
-    Udp.read(packetBuffer, NTP_PACKET_SIZE);
-
-    // the timestamp starts at byte 40 of the received packet and is four bytes,
-    // or two words, long. First, esxtract the two words:
-
-    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
-    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
-    // combine the four bytes (two words) into a long integer
-    // this is NTP time (seconds since Jan 1 1900):
-    unsigned long secsSince1900 = highWord << 16 | lowWord;
-    Serial.print("Seconds since Jan 1 1900 = ");
-    Serial.println(secsSince1900);
-
-    // now convert NTP time into everyday time:
-    Serial.print("Unix time = ");
-    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
-    const unsigned long seventyYears = 2208988800UL;
-    // subtract seventy years:
-    unsigned long epoch = secsSince1900 - seventyYears + 28800L;
-    // print Unix time:
-    Serial.println(epoch);
-
-
-    // print the hour, minute and second:
-    Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
-    Serial.print(((epoch  % 86400L) / 3600)); // print the hour (86400 equals secs per day)
-    Serial.print(':');
-    if (((epoch % 3600) / 60) < 10) {
-      // In the first 10 minutes of each hour, we'll want a leading '0'
-      Serial.print('0');
-    }
-    Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
-    Serial.print(':');
-    if ((epoch % 60) < 10) {
-      // In the first 10 seconds of each minute, we'll want a leading '0'
-      Serial.print('0');
-    }
-    Serial.println(epoch % 60); // print the second
-  }
+  printDateTime(getUnixTime());
   // wait ten seconds before asking for the time again
   delay(10000);
 }
@@ -132,4 +66,84 @@ void sendNTPpacket(char *ntpSrv)
   Udp.write(packetBuffer, NTP_PACKET_SIZE);
 
   Udp.endPacket();
+}
+
+void InitWiFi() {
+  int status = WL_IDLE_STATUS;     // the Wifi radio's status
+  // initialize ESP module
+  WiFi.init(&Serial1);
+  // check for the presence of the shield
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+
+    while (true);
+  }
+
+  Serial.println("Connecting to AP ...");
+
+  // attempt to connect to WiFi network
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid, pass);
+    delay(500);
+  }
+
+  // you're connected now, so print out the data
+  Serial.println("Connected to AP");
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP: ");
+  Serial.println(ip);
+}
+
+unsigned long getUnixTime() {
+  sendNTPpacket(timeServer); // send an NTP packet to a time server
+  
+  // wait for a reply for UDP_TIMEOUT miliseconds
+  unsigned long startMs = millis();
+  while (!Udp.available() && (millis() - startMs) < UDP_TIMEOUT) {}
+
+  Serial.println(Udp.parsePacket());
+  if (Udp.parsePacket()) {
+    Serial.println("packet received");
+    // We've received a packet, read the data from it into the buffer
+    Udp.read(packetBuffer, NTP_PACKET_SIZE);
+
+    // the timestamp starts at byte 40 of the received packet and is four bytes,
+    // or two words, long. First, esxtract the two words:
+
+    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+    // combine the four bytes (two words) into a long integer
+    // this is NTP time (seconds since Jan 1 1900):
+    unsigned long secsSince1900 = highWord << 16 | lowWord;
+
+    // now convert NTP time into everyday time:
+    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+    const unsigned long seventyYears = 2208988800UL;
+    //Taiwan time different in seconds, that's 28800:
+    const long taiwanTimeDifferent = 28800L;
+    // subtract seventy years and time different:
+    unsigned long epoch = secsSince1900 - seventyYears + taiwanTimeDifferent;
+    // return Unix time:
+    return epoch;
+  }
+}
+
+void printDateTime(unsigned long epoch) {
+  // print the hour, minute and second:
+  Serial.print("The Taiwan time is ");
+  Serial.print(((epoch  % 86400L) / 3600)); // print the hour (86400 equals secs per day)
+  Serial.print(':');
+  if (((epoch % 3600) / 60) < 10) {
+    // In the first 10 minutes of each hour, we'll want a leading '0'
+    Serial.print('0');
+  }
+  Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
+  Serial.print(':');
+  if ((epoch % 60) < 10) {
+    // In the first 10 seconds of each minute, we'll want a leading '0'
+    Serial.print('0');
+  }
+  Serial.println(epoch % 60); // print the second
 }
--- README.md
+++ README.md
@@ -3,5 +3,20 @@
 >>整個系統以arduino、app inventer為主，其中Thingspeak作為溝通
 
 ##Arduino Part
+###Method
 
+1. setup()
+	* initialize serial
+2. loop()
+	* just a loop XD
+3. sendNTPpacket(char *ntpSrv)
+	* send the UDP packet to the NTP sever
+4. InitWiFi()
+	* initialize ESP
+5. getUnixTime()
+	* send and recieve the UDP packet
+	* turn UTC time to taiwan time
+6. printDateTime(unsigned long epoch)
+	* print the Taiwan time
+	
 [參考資料](https://github.com/bportaluri/WiFiEsp/blob/master/examples/UdpNTPClient/UdpNTPClient.ino "Title")
